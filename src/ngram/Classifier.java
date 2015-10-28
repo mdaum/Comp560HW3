30002a852506348ac494387c51d376b06ba18a9e
unknown <daumisgod@gmail.com>
2015-10-28 14:16:26 -0400

    attempt at ngrams implementation

---
+++ src/ngram/Classifier.java
@@ -0,0 +1,94 @@
+package ngram;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Classifier {
+	int numDistinctWords=0,totalWords=0,numClassFiles;
+	double k,m;
+	String filepath;
+	HashMap<String, Integer> hashbrown= new HashMap<String, Integer>();
+	HashMap<String,Integer> refinedHashbrown= new HashMap<String,Integer>();
+	HashMap<String, Double> murmaider = new HashMap<String, Double>();
+	boolean windows;
+	int gram;
+
+	public Classifier(double k, double m, String filepath,int gram) {
+		this.m = m;
+		this.k = k;
+		this.gram=gram;
+		this.filepath = filepath;
+		this.windows=System.getProperty("os.name").toLowerCase().contains("win");
+	}
+
+	public void LexiconIT() {
+		try {
+			Scanner reader = new Scanner(new File(filepath));
+			//int i=0;
+			while(reader.hasNextLine()){
+				String nextFilePath=reader.nextLine();
+				//System.out.println("File "+i);
+				if(nextFilePath.length()>3){//check to see if it's just an empty new line character
+					numClassFiles++;
+					//all logic for while loop in here
+					if(windows)nextFilePath.replaceAll("\\\\","/");
+					Scanner innerRead=new Scanner(new File(nextFilePath));
+					String file="";
+					while(innerRead.hasNextLine()){
+						file+=innerRead.nextLine()+"\n";
+					}
+					/*String itemToHash=innerRead.next();
+					if(itemToHash.contains("\n"))itemToHash=itemToHash.substring(0, itemToHash.length()-1);//sanitized!
+					hashbrown.put(itemToHash, hashbrown.get(itemToHash)==null?1:hashbrown.get(itemToHash)+1);*/
+					file=file.replaceAll("\n"," ");
+					String tokens[]=file.split(" ");
+					
+					for(int i=0;i<tokens.length-gram+1;i++){
+						//here append next n(gram) tokens, starting from i, to a string which we will hash (string is space-delimited)
+						String toHash = "";
+						for(int j=0;j<gram;j++){
+							if(j!=gram-1)toHash+=tokens[i+j]+" ";
+							else toHash+=tokens[i+j];
+						}
+						hashbrown.put(toHash, hashbrown.get(toHash)==null?1:hashbrown.get(toHash)+1);
+						totalWords++;
+					}
+					innerRead.close();
+				}
+				//stupid EOF
+				//i++;
+			}
+			//sSystem.out.println("done hashing everything");
+			//System.out.println(hashbrown.containsKey("the"));
+			//System.out.println(hashbrown.get("buy"));
+			numDistinctWords=hashbrown.keySet().size();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+	public void filterHashMap(){
+		refinedHashbrown = new HashMap<String,Integer>();
+		for(String key : hashbrown.keySet()){
+			if(hashbrown.get(key)>k)
+				refinedHashbrown.put(key, hashbrown.get(key));
+		}
+	}
+	public void computeProbabilities(int goo){
+		for(String key : refinedHashbrown.keySet()){
+			murmaider.put(key, ((double)(refinedHashbrown.get(key)+m))/((double)(goo*(m+1))));
+		}
+	}
+	public double probabilityOfClass(String filepath,int goo) throws FileNotFoundException{
+		double probability=Math.log(.5);//yes, this IS the probability that it is either spam or ham based on what we observed from the training set
+		Scanner reader = new Scanner(new File(filepath));
+		while(reader.hasNext()){
+			String next = reader.next();
+			//System.out.println(next);
+			//System.out.println("probability that "+next+" belongs to whatever class this is:"+(probabilityHash.get(next)==null?(m/((m+1)*v)):probabilityHash.get(next)));
+			probability+=Math.log(murmaider.get(next)==null?(m/((m+1)*(double)goo)):murmaider.get(next));
+		}
+		return probability;
+	}
+	
+}
---
+++ src/ngram/Runner.java
@@ -0,0 +1,72 @@
+package ngram;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class Runner {
+	static int totalwords=0;
+	//NOTE: we need to calculate the probability of 
+	public static void main(String[] args) {
+		Scanner keyboard= new Scanner(System.in);
+		System.out.println("what gram?");
+		int gram=keyboard.nextInt();
+		double k = 6;//20 looks like a high
+		double m = 1;//1 is 91 with k=20. NOTE: 0 results in 0s. Much break. 
+		Classifier spamClassifier = new Classifier(k,m,"spamtraining.txt",gram);
+		spamClassifier.LexiconIT();
+		totalwords+=spamClassifier.totalWords;
+		Classifier hamClassifier = new Classifier(k,m,"hamtraining.txt",gram);
+		hamClassifier.LexiconIT();
+		totalwords+=hamClassifier.totalWords;
+		spamClassifier.filterHashMap();
+		hamClassifier.filterHashMap();
+		spamClassifier.computeProbabilities(totalwords);
+		hamClassifier.computeProbabilities(totalwords);
+		try{
+			printTestResults(spamClassifier,hamClassifier,"spamtesting.txt");
+			System.out.println("NOW FOR HAM");
+			printTestResults(spamClassifier,hamClassifier,"hamtesting.txt");
+		}
+		catch(FileNotFoundException f){
+			f.printStackTrace();
+		}
+		/*for(String key : spamClassifier.murmaider.keySet()){
+			int count=spamClassifier.hashbrown.get(key);
+			System.out.println(key+"   "+count);
+		}
+		System.out.println("AND NOW FOR HAM");
+		for(String key : hamClassifier.murmaider.keySet()){
+			int count=hamClassifier.hashbrown.get(key);
+			System.out.println(key+"   "+count);
+		}*/
+		
+		/*int max=0;
+		String keyMax="";
+		for(String key:spamClassifier.refinedHashbrown.keySet()){
+			if(spamClassifier.refinedHashbrown.get(key)>max){
+				max=spamClassifier.refinedHashbrown.get(key);
+				keyMax=key;
+			}
+		}
+		System.out.println(keyMax+" "+max);*/
+	}
+	//ACHTUNG: PROBABILITIES ARE NOT LOGS. YOU JUST GET A BUNCH OF NEGATIVE VALUES FROM LOGS
+	//TAKE THE LEAST NEGATIVE VALUE
+	public static void printTestResults(Classifier spamClassifier, Classifier hamClassifier, String testingDocFilePath) throws FileNotFoundException{
+		Scanner reader = new Scanner(new File(testingDocFilePath));
+		int i=0;
+		while(reader.hasNext()){
+			String nextTestingDoc = reader.next();
+			//System.out.println(nextTrainingDoc+": "+probabilityOfClass(m,v,probabilityHash,nextTrainingDoc));
+			double probSpam = spamClassifier.probabilityOfClass(nextTestingDoc,totalwords);
+			double probHam = hamClassifier.probabilityOfClass(nextTestingDoc,totalwords);
+			//System.out.println(nextTestingDoc+": "+(probSpam>probHam?"SPAAAAAM":"haaaaam"));
+			//only use this if we're expecting spam. In other words, only do this if we are using the spam testing set
+			i=probSpam>probHam?i+1:i;
+		}	
+		System.out.println("percent of contents classified as spam: "+i);
+		System.out.println("percent of contents classified as ham: "+(100-i));
+	}
+}
